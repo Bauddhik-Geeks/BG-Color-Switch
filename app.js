c630f2f44bbcf9566aa7d6c430357655750c6f13
Samiksha Sankar <samikshasankar2000@gmail.com>
2021-10-02 21:28:11 +0530

    Give user access to select the color that they want to change in background

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-
+/*
 let color = ["#40E0D0","#FF7F50","#6495ED","#FFBF00","#DE3163","#FFA600","#B200FF","#FF005D","#5694EE","#72E824"];
 let i = 0;
-document.querySelector("a").addEventListener("click", () => {
+document.querySelector("p").addEventListener("click", () => {
     i = i < color.length ? ++i : 0;
     document.querySelector("body").style.background = color[i];
 });
 
+*/
 const button = document.querySelector('a');
 button.addEventListener('click', function(e){
 
@@ -21,3 +22,11 @@ button.addEventListener('click', function(e){
          ripples.remove()
         },1000);
      })
+
+function userInputColor(){
+    let color = document.getElementById("input").value;
+    document.body.style.backgroundColor = color;
+    document.getElementById("input").value = ''
+}
+
+
